bf9209a3366acb3437a540d66f07610b23b47d90
Marcus <marcus.gamboa@gmail.com>
2021-08-16 13:05:24 -0600

    add hemlet and compression dependenceies

--- app.js
+++ app.js
@@ -8,13 +8,18 @@ var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var catalogRouter = require("./routes/catalog"); //Import routes for "catalog" area of site
 
+var compression = require("compression");
+var helmet = require("helmet");
+
 var app = express();
 
-//Set up mongoose connection
-var uri = require("./ignore.js");
+// Set up mongoose connection
 var mongoose = require("mongoose");
-var mongoDB = uri;
+var dev_db_url =
+  "mongodb+srv://cooluser:coolpassword@cluster0.a9azn.mongodb.net/local_library?retryWrites=true";
+var mongoDB = process.env.MONGODB_URI || dev_db_url;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = global.Promise;
 var db = mongoose.connection;
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
@@ -26,6 +31,9 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(helmet());
+app.use(compression()); // Compress all routes
+
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
